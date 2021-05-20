9d48e31a63648347b5960e8c2640cd52596a7fc8
Aleksandr Pisarik <aleksandr_pisarik@epam.com>
2021-05-20 12:57:32 +0300

    refactor: add dotenv to project

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const Sequelize = require('sequelize');
-//database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+require('dotenv').config();
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+  }
+);
 
 sequelize.authenticate().then(
   function success() {
